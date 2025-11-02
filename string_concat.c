d7cf4c68161fce908971b4e1aa6accd7a5e9ba15
Raseduzzaman-Rayhan <raseduzzamanrayhan@gmail.com>
2025-11-03 01:01:23 +0600

    learned how to use strcat to concat two string

--- string_concat.c
+++ string_concat.c
@@ -1,15 +1,29 @@
+// #include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+//     char a[101], b[101];
+//     scanf("%s %s", &a, &b);
+//     int length_a = strlen(a);
+//     int length_b = strlen(b);
+//     for (int i = 0; i <= length_b; i++)
+//     {
+//         a[i + length_a] = b[i];
+//     }
+
+//     printf("%s %s", a, b);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 int main()
 {
     char a[101], b[101];
     scanf("%s %s", &a, &b);
-    int length_a = strlen(a);
-    int length_b = strlen(b);
-    for (int i = 0; i <= length_b; i++)
-    {
-        a[i + length_a] = b[i];
-    }
+
+    strcat(a, b);
 
     printf("%s %s", a, b);
 
